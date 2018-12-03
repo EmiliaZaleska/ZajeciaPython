34016951d360091259d4db78e644e4b0d99723f1
Emilka <zaleska_emilia@wp.pl>
2018-12-03 21:22:06 +0100

    Commit - correcting 3.3

--- spotkanie3/zajecia3_zad3.py
+++ spotkanie3/zajecia3_zad3.py
@@ -1,15 +1,18 @@
+import re
 nazwa_pliku = "test.txt"
 
 
 def statystyka(nazwa_pliku):
-    with open(nazwa_pliku, 'r') as fh:
-        tekst = fh.read()
-        liczba_znakow = "Liczba znaków: " + str(len(tekst))
-        liczba_slow = "Liczba słów: " + str(len(tekst.split()))
-        if str(tekst.split(".")[-1]) == "":
-            liczba_zdan = "Liczba zdań: " + str(int(len(tekst.split("."))) - 1)
-        else:
-            liczba_zdan = "Liczba zdań: " + str(len(tekst.split(".")))
-        return liczba_znakow, liczba_slow, liczba_zdan
+    with open(nazwa_pliku, 'r') as file:
+        tekst = file.read()
+
+    liczba_znakow = "Liczba znaków: " + str(len(tekst))
+    liczba_slow = "Liczba słów: " + str(len(tekst.split()))
+    if str(re.split(r"[.!?]", tekst)[-1]) == "":
+        liczba_zdan = "Liczba zdań: " + str(int(len(re.split(r"[.!?]", tekst))) - 1)
+    else:
+        liczba_zdan = "Liczba zdań: " + str(len(re.split(r"[.!?]", tekst)))
+    return liczba_znakow, liczba_slow, liczba_zdan
+
 
 print(statystyka(nazwa_pliku))
